c3e2d61acb50badc9d0bd96faa9bc9973bb73583
d4niel <olayajuandaniel8@gmail.com>
2023-10-11 22:11:07 -0500

    class css

--- src/components/TableBoard.svelte
+++ src/components/TableBoard.svelte
@@ -1,4 +1,4 @@
-<div class="w-full p-2 "> 
+<div class="w-full p-2"> 
     <div class="code-container w-full h-80">
 
     </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,8 @@
 
     
       <TableBoard/>
+
+    
   
   
   <style lang="postcss">
