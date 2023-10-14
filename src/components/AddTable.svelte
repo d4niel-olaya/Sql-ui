fd5d0e3349f31f0c5d1451404b5b78d0124de0d6
d4niel <olayajuandaniel8@gmail.com>
2023-10-14 17:43:29 -0500

    tables

--- src/components/AddTable.svelte
+++ src/components/AddTable.svelte
@@ -18,17 +18,54 @@
         </button>
         
     </div>
+    
 
-    <dialog class="modal" bind:this={modal}>
-        <div class="modal-box">
-          <h3 class="font-bold text-lg">Hello!</h3>
-          <p class="py-4">Press ESC key or click the button below to close</p>
-          <div class="modal-action">
-            <form method="dialog">
-              <button class="btn">Close</button>
-            </form>
-          </div>
-        </div>
-      </dialog>
+        <dialog class="modal" bind:this={modal} >
+            <div class="modal-box">
+                <div class="form-control w-full">
+                    <!-- svelte-ignore a11y-label-has-associated-control -->
+                    <label class="label">
+                        <span class="label-text">Table name</span>
+                    </label>
+                    <input type="text" placeholder="MyTable" class="input input-bordered w-full max-w-xs" />
+                    <div class="flex flex-row w-full">
+                        <!-- svelte-ignore a11y-label-has-associated-control -->
+                        <div>
+                            <label class="label">
+                                <span class="label-text">column name</span>
+                            </label>
+                            <input type="text" placeholder="id" class="input input-bordered max-w-xs" />
+                        </div>
+                        <div>
+                            <!-- svelte-ignore a11y-label-has-associated-control -->
+                            <label class="label">
+                                <span class="label-text">column name</span>
+                            </label>
+                            <input type="text" placeholder="id" class="input input-bordered max-w-xs" />
+                        </div>
+                        <div>
+                            <!-- svelte-ignore a11y-label-has-associated-control -->
+                            <label class="label">
+                                <span class="label-text">column name</span>
+                            </label>
+                            <input type="text" placeholder="id" class="input input-bordered max-w-xs" />
+                        </div>
+                        <div>
+                            <!-- svelte-ignore a11y-label-has-associated-control -->
+                            <label class="label">
+                                <span class="label-text">column name</span>
+                            </label>
+                            <input type="text" placeholder="id" class="input input-bordered max-w-xs" />
+                        </div>
+                    </div>
+                </div>
+              <div class="modal-action">
+                <form method="dialog">
+                  <button class="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
+   
 
 
