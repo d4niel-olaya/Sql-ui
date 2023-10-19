f5c95e65a59280eacdea476dc3458d8ff8858a76
d4niel <olayajuandaniel8@gmail.com>
2023-10-18 22:18:30 -0500

    enums

---
+++ src/interfaces/enumsql.ts
@@ -0,0 +1,23 @@
+
+
+enum columnTypes
+{
+    BIGINT = 'bigint',
+    INT = 'int',
+    VARCHAR = 'varchar',
+    DATE = 'date',
+    FLOAT = 'float',
+    BOOLEAN = 'boolean',
+    TEXT = 'text',
+    DECIMAL = 'decimal',
+    CHAR = 'char',
+}
+
+
+enum columnConstraints
+{
+    NOT_NULL = 'NOT NULL',
+    UNIQUE = 'UNIQUE',
+    PRIMARY_KEY = 'PRIMARY KEY',
+    FOREIGN_KEY = 'FOREIGN KEY',
+}
