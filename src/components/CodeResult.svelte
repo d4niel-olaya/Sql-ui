0ff91b5d7d93fed4ae6d4edf1588dde44e62f6ad
d4niel <olayajuandaniel8@gmail.com>
2023-10-20 21:29:25 -0500

    textarea h

--- src/components/CodeResult.svelte
+++ src/components/CodeResult.svelte
@@ -4,6 +4,6 @@
 
 <div class="w-6/12 max-h-screen">
    <div class="mt-2 mr-2 ml-2 mb-2 overflow-y-auto h-56 p-2 max-h-56  rounded-md" id="board">
-      <textarea class="textarea textarea-bordered w-full h-52 w-6/12" bind:value={$script}></textarea>
+      <textarea class="textarea textarea-bordered w-full h-48 w-6/12" bind:value={$script}></textarea>
    </div>
 </div>
