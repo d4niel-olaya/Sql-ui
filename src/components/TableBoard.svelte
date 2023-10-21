3cefe953c6a01ba658869de84a35c0bb33c9ec1d
d4niel <olayajuandaniel8@gmail.com>
2023-10-20 23:19:55 -0500

    canvas test

--- src/components/TableBoard.svelte
+++ src/components/TableBoard.svelte
@@ -5,18 +5,30 @@
     import { storageService } from "../services/storageService";
     import { listTables } from "../services/list";
     const service = new storageService()
+    let canvas : HTMLCanvasElement
+    
     function loadTables() : ITable[]
     {
         const service = new storageService()
         return service.get()
     }
+
+    function click()
+    {
+        let ctx = canvas.getContext("2d");
+        ctx?.moveTo(10,10);
+        ctx?.lineTo(180,180);
+        ctx?.stroke();
+    }
     
 </script>
 
 
-
 <div class="w-full mt-2 overflow-hidden bg-slate-700 h-screen"> 
-    <div class="form-control w-44">
+    <canvas id="canvas" class="absolute w-full  h-screen" bind:this={canvas}>
+
+      </canvas>
+    <div class="absolute form-control w-44">
         <label class="cursor-pointer label">
             <input type="checkbox" class="toggle toggle-primary" checked disabled/>
             <span class="label-text">Show table details</span> 
